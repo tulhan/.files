29a7f51f14388a6ab402b9a469919a194d62ab91
Naresh Annangar <hi@tulhan.in>
2019-05-07 16:17:52 +0530

    Add base16-shell and fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,7 @@
+umask 077 # WSL: Default umask is not applied (github:Microsoft/WSL/issues/352)
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
