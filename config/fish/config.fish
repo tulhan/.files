287536a7b86d0aed52e1cc1f2f2eb275d4da903a
Naresh Annangar <hi@tulhan.in>
2019-05-07 16:32:27 +0530

    Add ~/bin to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,7 @@
 umask 077 # WSL: Default umask is not applied (github:Microsoft/WSL/issues/352)
 
+set -x PATH $PATH ~/bin
+
 # Base16 Shell
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
