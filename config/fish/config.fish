9b72c6ef35fdda0ae5b02dec0e07f06657f9ea21
Naresh Annangar <hi@tulhan.in>
2019-06-21 19:25:49 +0530

    Set fish in $SHELL

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,23 @@
 umask 077 # WSL: Default umask is not applied (github:Microsoft/WSL/issues/352)
 
 set -x PATH $PATH ~/bin
+set -x SHELL (which fish)
+
+# github:danhper/fish-ssh-agent
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
 
 # Base16 Shell
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 end
+
+# THEME PURE #
+set fish_function_path /home/nannangar/.config/fish/functions/theme-pure/functions/ $fish_function_path
+source /home/nannangar/.config/fish/functions/theme-pure/conf.d/pure.fish
