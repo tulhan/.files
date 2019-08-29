d081c32d708267499ace664edb23c37a51c26492
Naresh Annangar <hi@tulhan.in>
2019-08-29 23:50:20 +0530

    Persist SSH-AGENT globally

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,8 +3,14 @@ umask 077 # WSL: Default umask is not applied (github:Microsoft/WSL/issues/352)
 set -x PATH $PATH ~/.bin
 set -x SHELL (which fish)
 
-# https://wiki.archlinux.org/index.php/Fish#Evaluate_ssh-agent
-eval (ssh-agent -c)
+# github:danhper/fish-ssh-agent/blob/master/init.fish
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
 
 # Base16 Shell
 if status --is-interactive
